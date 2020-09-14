316c1426119f36bc4a4b56cf22538715acb5b0d1
IlijazM <35637072+IlijazM@users.noreply.github.com>
2020-09-14 12:07:40 +0200

    Added scoped styles

--- scripts/inline-script.js
+++ scripts/inline-script.js
@@ -1,3 +1,60 @@
+//#region scoped css
+function scope(scope, css) {
+    css = css.replace(/\/\*([^*]|[\r\n]|(\*+([^*\/]|[\r\n])))*\*\/+/g, "")
+
+    const regex = /([^\r\n,{}]+)(,(?=[^}]*{)|\s*{)/g
+    let m
+    let replaceList = []
+
+    while ((m = regex.exec(css)) !== null) {
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++
+        }
+
+        let match = m[0]
+        const index = m.index
+        match = match.trim()
+        if (!match.startsWith('@') && !match.startsWith('from') && !match.startsWith('to') && !/[\d]/.test(match.substr(0, 1))) {
+            let end = css.substr(index).trim()
+            if (end.startsWith('#this')) end = end.substr(6)
+            css = css.substring(0, index) + scope + ' ' + end
+            regex.lastIndex += scope.length + 1
+        }
+    }
+
+    return css
+}
+
+let SCOPEDCSSID = 0
+function getUniqueStyleId() { return 'scoped-css-id-' + SCOPEDCSSID++ }
+
+function scopeStyles(element) {
+    let styles = Array.from(element.querySelectorAll('style[scoped]'))
+
+    styles.forEach((styleElement) => {
+        const parentElement = styleElement.parentElement
+        const parentClass = getUniqueStyleId()
+        parentElement.classList.add(parentClass)
+
+        const style = scope("." + parentClass, styleElement.innerHTML)
+        styleElement.innerHTML = style
+        styleElement.removeAttribute('scoped')
+    })
+
+    styles = Array.from(element.querySelectorAll('style[scope]'))
+
+    styles.forEach((styleElement) => {
+        const style = scope(styleElement.attributes.getNamedItem('scope').value, styleElement.innerHTML)
+        styleElement.innerHTML = style
+        styleElement.removeAttribute('scope')
+    })
+}
+
+function scopeAllStyles() { scopeStyles(document.querySelector('html')) }
+
+scopeAllStyles()
+//#endregion
+
 // Custom properties
 
 HTMLElement.prototype.render = function () {
@@ -55,6 +112,7 @@ function load(element, url, args) {
 
             element.innerHTML = ""
             element.append(child)
+            scopeStyles(element)
         }
     }
     xmlHttp.open("GET", url, true)
@@ -72,6 +130,7 @@ function convertEvalResults(element, result) {
 
     if (result instanceof HTMLElement) {
         element.append(result)
+        scopeStyles(element)
         return element
     }
 
@@ -80,6 +139,7 @@ function convertEvalResults(element, result) {
 
         if (isListHTMLElement) {
             result.forEach(child => element.append(child))
+            scopeStyles(element)
             return element
         }
 
