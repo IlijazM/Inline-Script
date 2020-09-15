c0e091b5e62b4e47f9335385ca0d5d308b9aa8c7
IlijazM <35637072+IlijazM@users.noreply.github.com>
2020-09-15 18:18:22 +0200

    Added button-syntax.html and html-syntax.html

--- scripts/app.js
+++ scripts/app.js
@@ -16,4 +16,6 @@ const learnPages = [
     { url: preUrl + '/learn/', name: 'Introduction' },
     { url: preUrl + '/learn/inline-script-syntax.html', name: 'Inline Script Syntax' },
     { url: preUrl + '/learn/updating-an-element.html', name: 'Updating an element' },
+    { url: preUrl + '/learn/button-syntax.html', name: 'Button syntax' },
+    { url: preUrl + '/learn/html-syntax.html', name: 'HTML Syntax' },
 ]
