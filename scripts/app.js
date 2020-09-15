e307318053b62b8f5def12da13c7596ef21a181b
IlijazM <35637072+IlijazM@users.noreply.github.com>
2020-09-15 15:31:44 +0200

    Moved learn.html

--- scripts/app.js
+++ scripts/app.js
@@ -8,12 +8,12 @@ if (document.location.host === "ilijazm.github.io") {
 const navigation = [
     { url: preUrl + '/index.html', altUrl: '/', name: 'Home' },
     { url: preUrl + '/showcase.html', name: 'Showcase' },
-    { url: preUrl + '/learn.html', name: 'Learn' },
+    { url: preUrl + '/learn/', name: 'Learn' },
     { url: preUrl + '/documentation.html', name: 'Documentation' },
 ]
 
 const learnPages = [
-    { url: preUrl + '/learn.html', name: 'Introduction' },
+    { url: preUrl + '/learn/', name: 'Introduction' },
     { url: preUrl + '/learn/inline-script-syntax.html', name: 'Inline Script Syntax' },
     { url: preUrl + '/learn/updating-an-element.html', name: 'Updating an element' },
 ]
